d850ccc6311acafd36f9e08bf95873bddb6499ec
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 18:54:40 -0500

    Create EmployeeTable.sql

---
+++ EmployeeTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employeeTable(
+	employeeID int PRIMARY KEY,
+	employeeFirstName varchar(MAX),
+	employeeLastName varchar(MAX),
+	employeeContactEmail varchar(MAX),
+	employeeContactPhone tinyint,
+	employeeStartDate date,
+	employeePosition varchar(MAX),
+);
