e3876096851a066405a5a6ca4ec82df532c9a0cb
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 19:03:20 -0500

    Create customerData.sql

---
+++ customerData.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customerData(
+	customerID INT,
+	customerFirstName Varchar(MAX),
+	 customerLastName Varchar(MAX),
+	customerPhoneNumber TINYINT ,
+	customerEmail Varchar(MAX) ,
+	lastAccountActivityDate date,
+	accountType varchar(MAX),
+	purchaseMethod varchar(MAX),
+	cardDetails tinyint 
+);
