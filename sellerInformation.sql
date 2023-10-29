3c97fa5b210308a2488d4c7ab88cdd6505d00a59
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 19:02:54 -0500

    Create sellerInformation.sql

---
+++ sellerInformation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sellerDetails(
+	sellerID int PRIMARY KEY,
+	sellerName varchar(MAX),
+	sellerDescription varchar(MAX),
+	sellerContactPhone int,
+	sellerContactEmail varchar(MAX),
+	sellerAccountActiveTime tinyint,
+);
