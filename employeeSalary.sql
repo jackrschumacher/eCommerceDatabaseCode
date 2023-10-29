98d681474af8048ed8e7b1865eb5a81794fcd363
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 19:01:55 -0500

    Delete employeeSalary.sql

--- employeeSalary.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE salaryTable(
-	employeeID int PRIMARY KEY,
-	lastPayDate date,
-	lastPayAmountGross smallmoney,
-	lastPayAmountNet smallmoney,
-	payPeriodStart date,
-	payPeriodEnd date,
-	payPeriodNotes varchar(MAX),
-);
