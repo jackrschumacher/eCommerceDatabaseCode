3752d8ea05ba415ede253c2cfdf36f7a6763b442
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-28 19:01:02 -0500

    Create productOverview.sql

---
+++ productOverview.sql
@@ -0,0 +1,8 @@
+CREATE TABLE productOverview(
+	productID int PRIMARY KEY,
+	productName varchar(MAX),
+	productDescription varchar(MAX),
+	sellerName varchar(MAX),
+	sellerContactPhone tinyint, 
+	sellerContactEmail varchar(MAX),
+);
