a1ec9f762bf389327d1371b1e05466f54a8e5f10
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-10-31 13:15:32 -0500

    Begin adding product details

--- productDetailInsertStatement.sql
+++ productDetailInsertStatement.sql
@@ -1,2 +1,14 @@
 INSERT INTO productDetail
-VALUES(001, 'iPad Air 5', 'iPad Air 5', 'Technology', 8.5, 10, 1, 'High Volume');
+VALUES
+  (001, 'iPad Air 5', 'iPad Air 5', 'Technology', 8.5, 10, 1, 'High Volume'),
+  (002, 'iPad Air 4', 'iPad Air 4', 'Technology', 6, 10, 1, 'High Volume'),
+	(003, 'iPad 10', 'iPad 10', 'Technology', 9.2, 10, 1, 'Entry level iPad. Extremley high volume'),
+	(004, 'iPad Pro 12.9 inch', 'iPad Pro 12.9 inch with M2 chip, FaceID, Liquid Retina XDR display, and 12 MP and 10 MP Facetime cameras.', 'Technology', 6, 10, 1, 'Lower volume than the 11 inch model'),
+	(005, 'iPad Pro 11 inch', 'iPad Pro with 11 inch display, M2 chip, dual cameras, Face ID', 'Technology', 6, 10, 1, 'Highest volume Ipad Pro.'),
+	(006, 'Apple MacBook Pro 14.2', 'MacBook Pro with M3 Chip, 14.2 inch display, 8 core CPU and 10 core GPU', 'Laptop Computers', 0, 10, 1, 'New Release. Not avaliable for pre-order'),
+	(007, 'LG Gram 14 inch', 'LG Gram Intel 13th Gen i7 Evo with Windows 11 and 32 GB, 1TB SSD', 'Laptop Computers', 8, 10, 1, 'Very popular laptop'),
+	(008, 'Lessons in Chemistry', 'A research chemist, absurdly self-assured and immune to social convention...', 'Literature', 8, 10, 1, 'Top of Amazon best sellers list'),
+	(009, 'Samsung Galaxy Z Fold 5', 'Factory unlocked Android smartphone 256 GB', 'Cell Phones', 8, 10, 1, 'Very popular'),
+	(010, 'Samsung Galaxy Z Fold 3', 'Factory unlocked Android smartphone 256 GB', 'Cell Phones', 7, 10, 3, 'Very popular'),
+	(011, 'iPhone 15 Pro Max', 'Apple iPhone 15 Pro Max 256 GB - Black Titanium', 'Cell Phones', 9, 10, 1, 'Very popular'),
+	(012, 'iMac', 'The worlds best all in one, now supercharged by the M3 Chip', 'Laptop Computers', 8, 10, 1, 'Very popular'),
