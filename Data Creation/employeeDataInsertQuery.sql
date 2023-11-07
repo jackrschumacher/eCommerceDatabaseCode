5bb881c487f8703169808b6bbad3047de01553a2
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-11-07 13:24:08 -0600

    Update employeeDataInsertQuery.sql

--- Data Creation/employeeDataInsertQuery.sql
+++ Data Creation/employeeDataInsertQuery.sql
@@ -1,2 +1,10 @@
 INSERT INTO employeeTable
 VALUES
+	(001, 'John', 'Smith', 'johnsmith@yahoo.com', '2005-10-11', 'Head of Shipping'),
+	(002, 'Jane', 'Smith', 'janesmith@yahoo.com', '2001-01-10', 'Head of HR'),
+	(003, 'Dave', 'Pietti', 'davepietti@aol.com', '1990-8-10', 'Head of Engineering'),
+	(004, 'Don', 'Jacobs', 'donjacobs@aol.com', '1980-8-10', 'Engineering VP'),
+	(005, 'Travis', 'Groat', 'travisgroat@aol.com', '2020-10-10', 'Terminated'),
+	(006, 'Adien', 'Pederau', 'apedrau@gmail.com', '2022-6-5', 'CFO'),
+	(007, 'Daniel', 'Marzahl', 'dmarzahl@gmail.com', '2019-5-10', 'COO'),
+	(008, 'Issac', 'Olson', 'issacolson@gmail.com', '2005-3-10', 'Engineering VP'),
