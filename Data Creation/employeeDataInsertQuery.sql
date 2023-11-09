c124f590d1f9e96d99b3c53f310ed23919ed6b85
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-11-09 12:40:40 -0600

    Add additional data for fake employees

--- Data Creation/employeeDataInsertQuery.sql
+++ Data Creation/employeeDataInsertQuery.sql
@@ -8,3 +8,11 @@ VALUES
 	(006, 'Adien', 'Pederau', 'apedrau@gmail.com', '2022-6-5', 'CFO'),
 	(007, 'Daniel', 'Marzahl', 'dmarzahl@gmail.com', '2019-5-10', 'COO'),
 	(008, 'Issac', 'Olson', 'issacolson@gmail.com', '2005-3-10', 'Engineering VP'),
+	(009, 'Zachary', 'Simonson', 'simozack@gmail.com', '1980-10-10', 'Testing manager'),
+	(010, 'Xavier', 'Haines', 'hainxavi@gmail.com', '1-10-1967','Testing VP'),
+	(011, 'Noah', 'Romero', 'romenoah@gmail.com', '9-1-2005','Testing VP'),
+	(012, 'Braeden', 'Hughes', 'hughbrae@gmail.com', '3-27-2008', 'Quality Control Manager'),
+	(013, 'Charles', 'Lang', 'langchar@yahoo.com', '2-9-2000', 'Night Shift Lead'),
+	(014, 'Charles', 'Jefferson', 'jeffchar@yahoo.com', '5-4-2022', 'Morning Shift Lead'),
+	(015, 'John', 'Appleseed', 'appljohn@yahoo.com', '3-10-2020', 'Afternoon Shift Lead'),
+	(016, 'Zachary', 'Adams', 'adamzach@gmail.com', '12-9-2010', 'Plant Manager');
