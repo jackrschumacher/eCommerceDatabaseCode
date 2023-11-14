ff6856cb53705ecf767084b62589f105c9ab8a0b
Jack Schumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-11-14 13:16:51 -0600

    Add database roles to specific tables

--- databaseRolesCreation.sql
+++ databaseRolesCreation.sql
@@ -3,3 +3,14 @@ GO
 
 CREATE ROLE accessAdmin AUTHORIZATION db_accessadmin;  
 GO
+
+CREATE ROLE dbOwner AUTHORIZATION db_owner;  
+GO
+
+CREATE ROLE hrEdit AUTHORIZATION db_datawriter; 
+GO
+GRANT SELECT ON employeeTable to hrEdit
+
+CREATE ROLE customerEdit AUTHORIZATION db_datawriter;
+GO
+GRANT SELECT ON customerData to customerEdit
